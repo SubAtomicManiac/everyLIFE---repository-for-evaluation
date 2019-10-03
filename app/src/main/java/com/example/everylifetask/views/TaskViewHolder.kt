88e05eeb3b4da30e2bbbfc22ec70aeb46a416a49
Samuel Taiwo <mrtaiwosam@gmail.com>
2019-10-03 03:39:19 +0100

    made a few tweaks

--- app/src/main/java/com/example/everylifetask/views/TaskViewHolder.kt
+++ app/src/main/java/com/example/everylifetask/views/TaskViewHolder.kt
@@ -2,8 +2,6 @@ package com.example.everylifetask.views
 
 import androidx.recyclerview.widget.RecyclerView
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import kotlinx.android.synthetic.main.task_row_item.view.*
 
 class TaskViewHolder(taskView: View) : RecyclerView.ViewHolder(taskView) {
